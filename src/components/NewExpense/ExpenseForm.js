2ab838e9f8474d07dfa4dab9f8c7b2a21affbbd1
Alberto Roldán <a-f-r@hotmail.es>
2021-05-31 18:02:41 +0200

    Store the user inputs in an object and clear the inputs after the form is submitted

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,28 +2,67 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 function ExpenseForm() {
-  const [title, setTitle] = useState("");
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
 
-  const handleChange = function (event) {
-    setTitle(event.target.value);
-    console.log(event.target.value);
+  const titleChangeHandler = function (event) {
+    setEnteredTitle(event.target.value);
+  };
+  const amountChangeHandler = function (event) {
+    setEnteredAmount(event.target.value);
+  };
+  const dateChangeHandler = function (event) {
+    setEnteredDate(event.target.value);
+  };
+
+  const submitHandler = function (e) {
+    e.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseData);
+
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
 
   return (
     <div>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
           <div className="new-expense__control">
             <label>Title</label>
-            <input type="text" onChange={handleChange} value={title} />
+            <input
+              type="text"
+              onChange={titleChangeHandler}
+              value={enteredTitle}
+            />
           </div>
           <div className="new-expense__control">
             <label>Amount</label>
-            <input type="number" min="0.01" step="0.01" />
+            <input
+              type="number"
+              min="0.01"
+              step="0.01"
+              onChange={amountChangeHandler}
+              value={enteredAmount}
+            />
           </div>
           <div className="new-expense__control">
             <label>Date</label>
-            <input type="date" min="2019-01-01" max="2022-12-31" />
+            <input
+              type="date"
+              min="2019-01-01"
+              max="2022-12-31"
+              onChange={dateChangeHandler}
+              value={enteredDate}
+            />
           </div>
         </div>
         <div className="new-expense__actions">
